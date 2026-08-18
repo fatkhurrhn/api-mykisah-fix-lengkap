f7d7e48000ec6e60c49205dc5c276019b00f5e48
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2026-08-18 20:19:58 +0700

    updated

--- backup/index.js
+++ backup/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Konfigurasi
-const BASE_URL = 'https://kuramanime.ing';
+const BASE_URL = 'https://s1.kuramalink.app';
 
 // ============================================================
 // ENDPOINT: /anime/latest (Versi CEPAT tanpa Puppeteer)
